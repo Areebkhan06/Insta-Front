c05e42af03370b64dc08a475bb7894087153abfe
Areeb khan <areebkhan4575@gmail.com>
2025-08-08 00:21:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,8 @@ const App = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/register", {
+      const response = await fetch("https://insta-backend-8gky.onrender.com/api/register", {
+
         method: "POST",
         headers: {
           "Content-Type": "application/json",
